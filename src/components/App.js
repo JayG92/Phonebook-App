73657e609d830b7ddc5e1a83c73bac8ed49294ef
JayG <jaypguevara@gmail.com>
2019-10-21 09:09:45 -0700

    added structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contacts from './Contacts';
+import './App.css';
+
+function App() {
+  return (
+  <div className="Phonebook App">
+    <Contacts />
+  </div>
+  );
+}
+export default App;
